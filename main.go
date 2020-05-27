07ff5b016f891fb76c1ce5abb6cb33bfdd62562b
Todd Birchard <toddbirchard@gmail.com>
2020-05-26 23:58:19 -0400

    Initial commit.

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"github.com/gorilla/mux"
+	"html/template"
+	"log"
+	"net/http"
+	"time"
+)
+
+// Dynamic template values
+type HomeMetaData struct {
+	Title      string
+	TagLine    string
+	SiteUrl    string
+	ShareImage string
+	Icon       string
+}
+
+// Render homepage
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("templates/index.html"))
+	data := HomeMetaData{
+		Title:      "Penguin Truth",
+		TagLine:    "Exposing the facts about penguins and their flightless origins.",
+		SiteUrl:    "https://penguintruth.org/",
+		ShareImage: "https://hackers-content.nyc3.digitaloceanspaces.com/sites/hustlers/img/hustlersshare@2x.jpg",
+		Icon:       "https://hackers-content.nyc3.digitaloceanspaces.com/sites/penguintruth/img/icon.png",
+	}
+	tmpl.Execute(w, data)
+}
+
+// Route declaration
+func Router() *mux.Router {
+	r := mux.NewRouter()
+	r.HandleFunc("/", indexHandler)
+	return r
+}
+
+// Initiate web server
+func main() {
+	router := Router()
+	srv := &http.Server{
+		Handler:      router,
+		Addr:         "127.0.0.1:9200",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Fatal(srv.ListenAndServe())
+}
